443094d9ffa7f375324b8b7f7d171d4dae72b1b4
davide vanoncini <davidevanoncini2003@gmail.com>
2021-09-13 01:16:11 +0200

    bug fixed and size of charts

--- front-end/js/fruitStats.js
+++ front-end/js/fruitStats.js
@@ -1,70 +1,13 @@
 google.load("visualization", "1", { packages: ["corechart", "table"] });
-google.setOnLoadCallback(drawSort);
+// google.setOnLoadCallback(drawSort);
 
-// let fruits = [];
 
-let fruits = [
-    {
-        "genus": "Malus",
-        "name": "Apple",
-        "id": 6,
-        "family": "Rosaceae",
-        "order": "Rosales",
-        "nutritions": {
-            "carbohydrates": 11.4,
-            "protein": 0.3,
-            "fat": 0.4,
-            "calories": 52,
-            "sugar": 10.3
-        }
-    },
-    {
-        "genus": "Prunus",
-        "name": "Apricot",
-        "id": 35,
-        "family": "Rosaceae",
-        "order": "Rosales",
-        "nutritions": {
-            "carbohydrates": 3.9,
-            "protein": 0.5,
-            "fat": 0.1,
-            "calories": 15,
-            "sugar": 3.2
-        }
-    },
-    {
-        "genus": "Musa",
-        "name": "Banana",
-        "id": 1,
-        "family": "Musaceae",
-        "order": "Zingiberales",
-        "nutritions": {
-            "carbohydrates": 22,
-            "protein": 1,
-            "fat": 0.2,
-            "calories": 96,
-            "sugar": 17.2
-        }
-    },
-    {
-        "genus": "Fragaria",
-        "name": "Blueberry",
-        "id": 33,
-        "family": "Rosaceae",
-        "order": "Rosales",
-        "nutritions": {
-            "carbohydrates": 5.5,
-            "protein": 0,
-            "fat": 0.4,
-            "calories": 29,
-            "sugar": 5.4
-        }
-    }
-]
+let fruits = [];
 
 async function fetchFruitJSON() {
     const response = await fetch('https://lore-images.herokuapp.com/fruit/all');
     fruits = await response.json();
+    drawSort()
 }
 
 function drawSort() {
@@ -110,7 +53,12 @@ function drawSort() {
     google.visualization.events.addListener(table, 'sort',
         function (event) {
             data.sort([{ column: event.column, desc: !event.ascending }]);
-            view.setColumns([0, event.column]);
+            if (event.column == 0) {
+                view.setColumns([0, 1]);
+
+            } else {
+                view.setColumns([0, event.column]);
+            }
             chart.draw(view);
         });
 }
